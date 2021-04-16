7b44951f48730e898de98f22bcd5a7fcf9b7aa5a
Sathya <sathyanan29@gmail.com>
2021-04-16 19:52:24 +0530

    included data model

---
+++ Shock.API/Shock.API/Data/ObservationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shock.API.DataModel;
+
+namespace Shock.API.Data
+{
+    public class ObservationDbContext: DbContext
+    {
+        public ObservationDbContext(DbContextOptions<ObservationDbContext> options) : base(options) { }
+
+        public DbSet<Observation> Observations { get; set; }
+    }
+}
---
+++ Shock.API/Shock.API/DataModel/Observation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shock.API.DataModel
+{
+    public class Observation
+    {
+        public string ReferenceNo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Category { get; set; }
+        public string TypeOfActivity { get; set; }
+        public string AgreedAction { get; set; }
+        public string Evidence { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectLocation { get; set; }
+    }
+}
