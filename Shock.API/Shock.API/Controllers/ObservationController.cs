99a3670f6243f474e73e97c4d28e6ad689cd4d7d
Marutharaj <marutharaj.mayilraj@syncfusion.com>
2021-04-22 00:10:15 +0530

    Shoc api changes committed for send mail

--- Shock.API/Shock.API/Controllers/ObservationController.cs
+++ Shock.API/Shock.API/Controllers/ObservationController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shock.API.Data;
 using Shock.API.DataModel;
+using System.Net;
+using System.Net.Mail;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -56,6 +58,7 @@ namespace Shock.API.Controllers
             {
                 dbContext.Observations.Add(observation);
                 dbContext.SaveChanges();
+                _ = SendMail(observation, true);
                 return new OkObjectResult(observation);
             }
             catch
@@ -63,6 +66,52 @@ namespace Shock.API.Controllers
                 return BadRequest("Something went wrong, Details not added");
             }
         }
+
+        [HttpPost("SendMail")]
+        public async Task SendMail(Observation observation, bool isUpdate)
+        {
+            var fromAddress = new MailAddress("shocobserver@gmail.com", "SHOC ADMIN");
+            var toAddress = new MailAddress("shocuser@gmail.com", "SHOC USERS");
+            const string fromPassword = "Shoc@123";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            var color = observation.Status == "Open" ? "red" : observation.Status == "Inprogress" ? "yellow" : "green";
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                IsBodyHtml = true,
+                Subject = isUpdate == true ? "Observation updated" : "New Observation added",
+                Body = " <br/> <b>Observation Name:</b> " + observation.Name + "<br/>" +
+                "<b>Project Name:</b> " + observation.ProjectName + "<br/>" +
+               //  "<b>Status :</b> <span style='color:"+color+"'" + observation.Status + "</span><br/>" +
+               "<b>Status :" + observation.Status + "<br/>" +
+                "<b>Project Location :" + observation.ProjectLocation + "<br/>" +
+                "<b>Activity:</b> " + observation.TypeOfActivity + "<br/>" +
+                "<b>Action Owner:</b> " + observation.ActionOwner + "<br/>" +
+                 "<b>Agreed Action:</b> " + observation.AgreedAction + "<br/>" +
+                "<b>Category:</b> " + observation.Category + "<br/>" +
+                "<b>Created User: " + observation.CreatedUser + "<br/>" +
+                "<b>Created Date:</b> " + observation.DateCreated + "<br/>" +
+                "<b>Description:</b> " + observation.Description + "<br/>" +
+                 "<b>Evidence:</b> " + observation.Evidence + "<br/>" +
+                 "<br/>" +
+                "<b>App Link:</b> <a href='https://shock-ui.herokuapp.com' target='_blank'> https://shock-ui.herokuapp.com</a> <br/>",
+            })
+
+            {
+               // message.To.Add("");
+                //message.To.Add("shocuser@gmail.com");
+                await smtp.SendMailAsync(message);
+            }
+        }
+
 
         // PUT: api/Quality/5
         [HttpPost("UpdateObservation")]
@@ -90,6 +139,7 @@ namespace Shock.API.Controllers
                     dbContext.Observations.Update(res);
                     dbContext.SaveChanges();
                 }
+                _ = SendMail(observation, true);
                 return new OkObjectResult(res);
             }
             catch (Exception ex)
